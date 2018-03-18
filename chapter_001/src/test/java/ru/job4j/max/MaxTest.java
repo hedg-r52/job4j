40bc6bdaa0fa2c04e4e2691a2e5ddfa8f3cd1340
Andrei Soloviev <hedg.r52@gmail.com>
2018-03-18 10:25:17 +0300

    4.2. Максимум из двух чисел

---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Andrei Soloviev (hedg.r52)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MaxTest {
+
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenFirstMoreSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 1);
+        assertThat(result, is(2));
+    }
+}
