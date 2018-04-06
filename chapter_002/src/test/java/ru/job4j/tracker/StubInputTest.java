933a959da0d6e60e20ccbd930d48af774dbe9cd1
Andrei Soloviev <hedg.r52@gmail.com>
2018-04-06 20:02:02 +0300

    6.1. Обеспечить бесперебойную работу приложения Tracker. Тест для StubInput

---
+++ chapter_002/src/test/java/ru/job4j/tracker/StubInputTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class StubInputTest {
+    private final ByteArrayOutputStream mem = new ByteArrayOutputStream();
+    private final PrintStream out = System.out;
+
+    @Test
+    public void whenAskOneFromMenuThenResultEqualsOne() {
+        StubInput input = new StubInput(new String[] {"1"});
+        Tracker tracker = new Tracker();
+        MenuTracker menu = new MenuTracker(input, tracker);
+        menu.fillActions();
+        int[] ranges = menu.getRange();
+        int result = input.ask("Enter", ranges);
+        assertThat(result, is(1));
+    }
+
+    @Before
+    public void loadMem() {
+        System.setOut(new PrintStream(this.mem));
+    }
+
+    @After
+    public void loadSys() {
+        System.setOut(this.out);
+    }
+
+
+
+}
