53a70a64509343623fecebaea04f7b6c9712d638
Andrei Soloviev <hedg.r52@gmail.com>
2018-04-19 22:22:41 +0300

    2. Написать программу преобразования List в Map

--- chapter_003/src/main/java/ru/job4j/list/UserConvert.java
+++ chapter_003/src/main/java/ru/job4j/list/UserConvert.java
@@ -5,20 +5,18 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * TODO description
+ * Конвертер list -> hashmap
  *
  * @author Andrei Soloviev (hedg.r52@gmail.com)
  * @version $Id$
  * @since 0.1
  */
 public class UserConvert {
-    private static int counter = 0;
-
     public HashMap<Integer, User> process(List<User> list) {
-        Map<Integer, User> result = new HashMap<>();
+        HashMap<Integer, User> result = new HashMap<>();
         for (User user : list) {
             result.put(user.getId(), user);
         }
-        return (HashMap) result;
+        return result;
     }
 }
