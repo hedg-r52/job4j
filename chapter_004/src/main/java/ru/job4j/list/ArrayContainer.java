491435d7d7cc64c850ab001c69a78e5e99230ec7
Andrei Soloviev <hedg.r52@gmail.com>
2018-04-30 16:02:58 +0300

    5.3.1. Создать динамический список на базе массива.

---
+++ chapter_004/src/main/java/ru/job4j/list/ArrayContainer.java
@@ -0,0 +1,58 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Контейнер на массиве
+ *
+ * @author Andrei Soloviev (hedg.r52@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayContainer<E> implements Iterable<E> {
+    private Object[] container;
+    private final static int DEFAULT_CONTAINER_SIZE = 10;
+    private int position;
+
+    public ArrayContainer() {
+        this(DEFAULT_CONTAINER_SIZE);
+    }
+
+    public ArrayContainer(int size) {
+        this.container = new Object[size];
+        position = 0;
+    }
+
+    public void add(E value) {
+        if (position >= this.container.length) {
+            growContainerSize();
+        }
+        this.container[position++] = value;
+    }
+
+    public E get(int index) {
+        return (E) this.container[index];
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return (index < position);
+            }
+
+            @Override
+            public E next() {
+                return (E) container[index++];
+            }
+        };
+    }
+
+    private void growContainerSize() {
+        this.container = Arrays.copyOf(this.container, this.container.length * 2);
+    }
+}
