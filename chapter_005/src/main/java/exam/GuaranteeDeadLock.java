8cfcb608bf11725deb4b040dcfaaa06e20b2a7a9
Andrei Soloviev <hedg.r52@gmail.com>
2018-11-27 16:03:47 +0300

    Гарантированный DeadLock. Updated: added common resources

--- chapter_005/src/main/java/exam/GuaranteeDeadLock.java
+++ chapter_005/src/main/java/exam/GuaranteeDeadLock.java
@@ -6,28 +6,40 @@ import java.util.concurrent.Executors;
 
 public class GuaranteeDeadLock {
     public static void main(String[] args) throws Exception {
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        CountDownLatch cdl = new CountDownLatch(2);
         ExecutorService exec = Executors.newCachedThreadPool();
-        CountDownLatch cdl = new CountDownLatch(10);
-        for (int i = 0; i < 5; i++) {
-            exec.execute(new Worker(cdl, "worker" + i));
-        }
-        cdl.await();
+        exec.execute(new Worker(cdl, obj1, obj2, "worker1"));
+        exec.execute(new Worker(cdl, obj2, obj1, "worker2"));
     }
 }
 
 class Worker implements Runnable {
-
     final CountDownLatch cdl;
+    Object obj1;
+    Object obj2;
     final String name;
 
-    public Worker(CountDownLatch cdl, String name) {
+    public Worker(CountDownLatch cdl, Object obj1, Object obj2, String name) {
         this.cdl = cdl;
+        this.obj1 = obj1;
+        this.obj2 = obj2;
         this.name = name;
     }
 
     @Override
     public void run() {
-        System.out.println("Worker#" + name + " do work.");
-        cdl.countDown();
+        synchronized (obj1) {
+            cdl.countDown();
+            try {
+                cdl.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (obj2) {
+                System.out.println("Worker#" + name + " do work.");
+            }
+        }
     }
 }
